d02b5ef238d9b8a0c5bbec35fe9f655880d9f5ea
JoaoVHC <joao.henriquez@outlook.com>
2023-12-05 17:52:28 -0300

    usando_flask.pytemplates/index.html

--- usando_flask.py
+++ usando_flask.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 def index():
     requisicao = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,CLP-BRL")
     dic_requisicao = requisicao.json()
+    titulo = "Bem vindo a cotação em tempo real de moedas"
     return render_template('index.html', clp_bid=dic_requisicao['CLPBRL']['bid'], usd_bid=dic_requisicao['USDBRL']['bid'])
 
 if __name__ == '__main__':
