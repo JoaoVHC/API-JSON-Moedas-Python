69857a0f945d76ac97867b2d55b608054ac5809a
JoaoVHC <joao.henriquez@outlook.com>
2023-12-05 21:08:18 -0300

    cotacao_moedas.py

--- cotacao_moedas.py
+++ cotacao_moedas.py
@@ -4,5 +4,5 @@ requisicao = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,CLP-B
 print(requisicao)
 print(requisicao.json())
 dic_requisicao = requisicao.json()
-print(f"Cotação do Peso Chileno para Real é: {dic_requisicao['CLPBRL']['bid']}")
-print(f"Cotação do Dolar para Real é: {dic_requisicao['USDBRL']['bid']}")
+print(f"Cotação de 1 Peso Chileno para Real é: {dic_requisicao['CLPBRL']['bid']}")
+print(f"Cotação de 1 Dolar para Real é: {dic_requisicao['USDBRL']['bid']}")
